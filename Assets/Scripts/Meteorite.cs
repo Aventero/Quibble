9e6a5cc2ccffb3ffbf201989978a56b0e36a1e7e
Aventero <henrikweidner98@gmail.com>
2022-11-09 19:59:52 +0100

    TRAILS

--- Assets/Scripts/Meteorite.cs
+++ Assets/Scripts/Meteorite.cs
@@ -4,27 +4,43 @@ using UnityEngine;
 
 public class Meteorite : MonoBehaviour
 {
+    public float ScalingFactor = 1.0f;
+    public float StartPower = 10.0f;
+    public float acceleration = 5.0f;
+    public float drag = 0.95f;
     private Transform gravitationCenter;
     private Rigidbody2D rb2D;
-    private float Acceleration = 5.0f;
     private Vector2 downVector;
+    
     // Start is called before the first frame update
     void Start()
     {
         gravitationCenter = GameObject.FindGameObjectWithTag("Planet").transform;
         rb2D = GetComponent<Rigidbody2D>();
-        rb2D.velocity = new Vector2(Random.Range(-2, 2), Random.Range(-2, 2));
+        transform.up = -(gravitationCenter.position - transform.position);
+
+        rb2D.velocity = transform.right * StartPower;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         // Calculate direction
-        this.transform.up = -(gravitationCenter.position - this.transform.position);
+        this.transform.up = -(gravitationCenter.position - transform.position);
         downVector = (gravitationCenter.transform.position - gameObject.transform.position).normalized;
         Debug.DrawRay(transform.position, downVector);
         Debug.DrawRay(transform.position, rb2D.velocity, Color.blue);
-        rb2D.velocity += downVector * Time.fixedDeltaTime * Acceleration;
+
+        // Gravitation
+        rb2D.velocity += downVector * Time.fixedDeltaTime * acceleration;
+        rb2D.velocity *= drag;
+
+        // Scale the meteorite by distance
+        float distance = Vector3.Distance(transform.position, gravitationCenter.position);
+        transform.localScale = Vector3.one * 1.0f / (distance + 1) * ScalingFactor;
+
+        // Lookat in 2D on pivot
+        transform.up = -(gravitationCenter.position - transform.position);
 
         if (Vector2.Distance(gravitationCenter.position, transform.position) >= 10.0f)
         {
