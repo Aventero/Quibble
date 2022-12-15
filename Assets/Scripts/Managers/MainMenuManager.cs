63c29e6e8ec832c9cf3b274dee5b7abf5090f107
Aventeroy <henrikweidner98@gmail.com>
2022-12-15 20:51:05 +0100

    Pause Menu Not Anymore .

--- Assets/Scripts/Managers/MainMenuManager.cs
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MainMenuManager : MonoBehaviour
 {
     public GameLoader gameLoader;
+    public GameObject Player;
+
+    private void Start()
+    {
+        Player.GetComponent<PlayerController>().PlayerControls.FindAction("Pause").Disable();
+    }
 
     public void StartGame()
     {
+        Player.GetComponent<PlayerController>().PlayerControls.FindAction("Pause").Enable();
         gameLoader.LoadGameScene();
     }
 
     public void LoadTutorial()
     {
+        Player.GetComponent<PlayerController>().PlayerControls.FindAction("Pause").Enable();
         gameLoader.LoadTutorialScene();
     }
 
